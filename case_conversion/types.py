28177a1136ab3214277d76b16269cfa5d3ef934d
Ben Felder <ben@felder.io>
2020-02-24 14:36:31 +0100

    Work on types

--- case_conversion/types.py
+++ case_conversion/types.py
@@ -1,28 +1,32 @@
-from enum import Enum, unique
+from enum import Enum, auto
 
 
 class InvalidAcronymError(Exception):
     """Raise when acronym fails validation."""
 
-    def __init__(self, acronym: str) -> None:
+    def __init__(self, acronym: str) -> None:  # noqa: D107
         msg = f"Case Conversion: acronym '{acronym}' is invalid."
         super().__init__(msg)
 
 
-@unique
 class Case(Enum):
-    # - upper: All words are upper-case.
-    # - lower: All words are lower-case.
-    # - pascal: All words are title-case or upper-case. Note that the
-    #           stringiable may still have separators.
-    # - camel: First word is lower-case, the rest are title-case or
-    #          upper-case. stringiable may still have separators.
-    # - mixed: Any other mixing of word casing. Never occurs if there are
-    #          no separators.
-    # - unknown: stringiable contains no words.
-    UNKOWN = "unknown"
-    UPPER = "upper"
-    LOWER = "lower"
-    CAMEL = "camel"
-    PASCAL = "pascal"
-    MIXED = "mixed"
+    """Enum representing case types.
+
+    Members:
+        UNKNOWN: String contains no words.
+        UPPER: All words are upper-case.
+        LOWER: All words are lower-case.
+        PASCAL: All words are title- or upper-case. (String may still
+         have separators.)
+        CAMEL: First word is lower-case, the rest are title- or
+         upper-case. (String may still have separators.)
+        MIXED: Any other mixing of word casing. Never occurs if there
+         are no separators.
+    """
+
+    UNKOWN = auto()
+    UPPER = auto()
+    LOWER = auto()
+    CAMEL = auto()
+    PASCAL = auto()
+    MIXED = auto()
